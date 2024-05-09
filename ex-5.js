059a99f3821a76a575ccc85d1926f195360e4aef
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-09 14:47:42 +0700

    feat:primrtive

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discountPercent = 0.25;
+let afterDiscountPrice = totalPrice - totalPrice * discountPercent;
+console.log(afterDiscountPrice);
--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,10 @@
 // Exercise #6: Displaying message
-
+//"Total: 13000 Machine parts"
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+let concatSentence =
+  "Total:" + " " + totalParts + " " + "Machine" + " " + "parts";
+let literalSentence = `Total: ${totalParts} Machine parts`;
+
+console.log(concatSentence);
+console.log(literalSentence);
